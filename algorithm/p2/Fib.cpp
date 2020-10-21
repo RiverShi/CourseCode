44ccf50b228cf9b579d942466b622cef152009b2
RiverShi <River_Shi@qq.com>
2020-10-21 23:45:24 +0800

    don't know how to classify

--- algorithm/p2/Fib.cpp
+++ algorithm/p2/Fib.cpp
@@ -5,12 +5,29 @@
 
 using namespace std;
 
+#define ldb long double
+#define uLL unsigned long long
 
 
+/*
+    大数也就图一乐,精度问题没法解决
+    不过只是比较时间复杂度倒是没什么
+    pow的参数类型是double，用 long double保存数据是没有意义的, 到 10^308也就inf了，精度也不会更高
+    不如就自然溢出吧
+*/
+double fib_GF(ldb n)
+{
+    static ldb sqrt_5 = sqrt(5);
+    return ( pow((1+sqrt_5)/2, n)-pow((1-sqrt_5)/2, n) ) / sqrt_5;
+}
 
+
+uLL fib[10000];
 
 
 int main()
 {
-
+    for(int i = 1; i <= 2000; i++)cout<<fib_GF(i)<<endl;
+    fib[1] = fib[2] = 1;
+    for(int i = 3; i <= 100; i++)fib[i] = fib[i-1] + fib[i-2],cout<<fib[i]<<endl;
 }
